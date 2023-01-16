406ae96811223dee4cd72db4304647ed2c6477a3
Amelgarejo6 <imsoyanthony6@gmail.com>
2023-01-16 14:11:23 -0800

    restaurant update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './portfolio.css';
 import LEXOLETICS from '../../assets/lexoletics.png';
 import ATHLEAD from '../../assets/athlead.png';
+import RESTAURANT from '../../assets/restaurant.png';
 
 const Portfolio = () => {
     return(
@@ -16,7 +17,7 @@ const Portfolio = () => {
                     <h3>Lexoletics Full Stack e-Commerce Application</h3>
                     <h3>Built with Python, Flask, MySQL</h3>
                     <div className="portfolio__item-cta">
-                        <a href='https://github.com/AMelgarejo6/Full_Stack.git' className='btn btn-primary'>GitHub</a>
+                        <a href='https://github.com/AMelgarejo6' className='btn btn-primary'>GitHub</a>
                     </div>
                 </article>
                 <article className='portfolio__item'> 
@@ -26,7 +27,17 @@ const Portfolio = () => {
                     <h3>Athlead Full Stack Meetup Application</h3>
                     <h3>Built with Java, Spring Boot, MySQL</h3>
                     <div className="portfolio__item-cta">
-                        <a href='https://github.com/AMelgarejo6/Java/tree/main/Athlead' className='btn btn-primary'>GitHub</a>
+                        <a href='https://github.com/AMelgarejo6' className='btn btn-primary'>GitHub</a>
+                    </div>
+                </article>
+                <article className='portfolio__item'> 
+                    <div className='portfolio__item-image'>
+                        <img src={RESTAURANT} alt='restaurant'/>
+                    </div>
+                    <h3>Modern UI/UX Restaurant Website</h3>
+                    <h3>Built with ReactJS, HTML, CSS</h3>
+                    <div className="portfolio__item-cta">
+                        <a href='https://amelgarejo6.github.io/restaurant-project/#menu' className='btn btn-primary'>See Here</a>
                     </div>
                 </article>
             </div>
